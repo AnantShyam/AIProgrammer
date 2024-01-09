e01c5bb78765c8c731a3bf03f0833f01dd1d55af
Anant Shyam <anantshyam19@gmail.com>
2024-01-09 15:12:38 -0500

    add code

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import dataset
 import numpy as np
 import keras_nlp
 from tqdm import tqdm
+from torch.utils.data import DataLoader
 import tensorflow as tf
 
 
@@ -19,17 +20,21 @@ class Model(torch.nn.Module):
             for j in range(self.length_sentence):
                 # check if word is an integer or not
                 word = self.sentences[i][j][:]
-                word = word[1:] if word[0] == '-' else word
+                word = word[1:] if word[0] == '-' else word  # handle negative integers
                 if word.isnumeric():
                     self.sentences[i][j] = 'Integer'
 
-        self.max_len = max([len(sentence) for sentence in self.sentences])
-        self.embeddings = gensim.models.Word2Vec(self.sentences, min_count=1, vector_size=self.max_len)
+        # self.max_len = max([len(sentence) for sentence in self.sentences])
+
+        self.embedding_size = 30  # can change this if needed
+        self.embeddings = gensim.models.Word2Vec(self.sentences, min_count=1, vector_size=self.embedding_size)
 
         self.word_trained_embeddings = {}
-        word_vocab = list(self.embeddings.wv.key_to_index.keys())
-        for word in word_vocab:
+        self.word_vocab = list(self.embeddings.wv.key_to_index.keys())
+
+        for word in self.word_vocab:
             self.word_trained_embeddings[word] = self.embeddings.wv[word]
+
 
         # add positional embeddings
         for word in self.word_trained_embeddings:
@@ -45,12 +50,17 @@ class Model(torch.nn.Module):
             new_word_embedding = torch.cat(tuple([i for i in new_word_embedding]), 0)
             self.word_trained_embeddings[word] = new_word_embedding
 
-        # for word in self.word_trained_embeddings:
-        #     print(word)
-        #     print(self.word_trained_embeddings[word])
-        #     print("----")
+        # print(self.word_trained_embeddings)
+        self.prepare_rnn_model_inputs()
 
-        self.rnn = torch.nn.RNN(input_size=self.max_len, hidden_size=30) # random initialization of RNN
+        self.num_layers = 10  # can change this if needed
+        self.hidden_size = len(self.word_vocab) # can change this if needed
+
+        self.train_data, self.initial_hidden_state = self.prepare_rnn_model_inputs()
+        self.train_dataloader = DataLoader(self.train_data, batch_size=32, shuffle=True)
+
+        self.rnn = torch.nn.RNN(input_size=30, hidden_size=self.hidden_size) # random initialization of RNN
+        self.loss_function = torch.nn.CrossEntropyLoss()
         self.optimizer = torch.optim.SGD(self.parameters(), lr=0.001)
 
 
@@ -73,12 +83,32 @@ class Model(torch.nn.Module):
         return sentence
 
 
+    def prepare_rnn_model_inputs(self):
+        # prepare the appropriate tensors to use to train the rnn
+        train_data_embeddings = []
+        for sentence in self.sentences:
+            sentence_embedding = []
+            for word in sentence:
+                embedding = self.word_trained_embeddings[word]
+                sentence_embedding.append(embedding)
+            sentence_embedding = np.array(sentence_embedding)
+            train_data_embeddings.append(sentence_embedding)
+        train_data_embeddings = torch.from_numpy(np.array(train_data_embeddings))
+
+        h_0 = torch.rand((self.num_layers, self.hidden_size))
+        return train_data_embeddings, h_0
+
+
     def forward(self, data):
         data = self.interpret_user_input(data)
         return self.rnn(data)
 
+
     def train_model(self):
-        pass
+        num_epochs = 100
+        for _ in tqdm(range(num_epochs)):
+            pass
+
 
     def test_model(self):
         pass
